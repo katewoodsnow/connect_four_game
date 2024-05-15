ce3296e8abc490eadef008868b0ca899894d356d
katewoodsnow <kate.wood@hotmail.co.uk>
2024-05-15 22:48:36 +0100

    Add method to switch players.

--- connect_four.py
+++ connect_four.py
@@ -13,7 +13,7 @@ class ConnectFour:
     def get_grid(self):
         return self.grid
 
-# Add a token to the game grid
+    # Add a token to the game grid
     def add_token(self, column, token):
         # Check to see if the column entered by the player is within the
         # grid bounds
@@ -21,10 +21,15 @@ class ConnectFour:
             if column < 0 or column >= self.columns:
                 print("Invalid column index. Please enter a valid column index.")
             else:
+                # Check row in reverse order as token drops to the bottom first
                 for row in range(self.rows - 1, -1, -1):
                     # Check to see if cell is empty
                     if self.grid[row][column] == ' ':
                         self.grid[row][column] = token
-                        return True  # Token added successfully
+                        # Token added successfully
+                        return True
                 print("Column is full. Please choose another column.")
 
+    # Switches the player either red or yellow
+    def switch_players(self):
+        self.current_player = 'Y' if self.current_player == 'R' else 'R'
