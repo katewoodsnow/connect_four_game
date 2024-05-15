a6d77bc868ab26a28a967e4fc06d827772e2cf3b
katewoodsnow <kate.wood@hotmail.co.uk>
2024-05-15 16:18:02 +0100

    Add class and methods from implement file to file.
    Test add token method added

--- test_connect_four.py
+++ test_connect_four.py
@@ -1,6 +1,9 @@
 # Tests the methods behave as expected using TDD. If the assertion passes,
 # it means the method is working correctly.
 
+# Import class and methods from implement file
+from connect_four import ConnectFour
+
 # Test for initializing the game grid and that it is empty of tokens
 def test_initialization():
     game = ConnectFour(rows=6, columns=7, win_length=4)
@@ -9,3 +12,12 @@ def test_initialization():
     assert game.win_length == 4
     assert game.current_player == 'R'
     assert game.grid == [[' ' for _ in range(7)] for _ in range(6)]
+
+# Test add token function by adding values.
+def test_add_token():
+    game = ConnectFour(rows=6, cols=7, win_length=4)
+    # Don't need to specify row as can only go to last row
+    assert game.add_token(0, 'R')
+    grid = game.get_grid()
+    # Ensures token is bottom row
+    assert grid[5][0] == 'R'
